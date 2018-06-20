264d5dc7c65a4e11215d2bb7c67bf1b0a410aa41
luri <luri@mujeresunidas.net>
2018-06-20 11:45:17 -0700

    JS random button completed

--- Assignments/Unit_2/Assignment_1/index.js
+++ Assignments/Unit_2/Assignment_1/index.js
@@ -2,26 +2,68 @@
 
 // 1. Perform three basic math operations
 
+let num1 = 5;
+num1 += 9;
+console.log(num1);
+
+let num2 = 34;
+num2 -= 1;
+console.log(num2);
+
+let num3 = 9;
+num3 *= 2;
+console.log(num3);
 
 
 // 2. Create a variable called "myNum" and assign it a value of 3
 
+let myNum = 3;
+
+
 // 3. Output/print/return the value of the "myNum" variable
+
+console.log(myNum);
+
 
 // 4. Assign "mynum" a value of 7
 
+let myNum1 = 7;
+
 // 5. Output/print/return the value of "myNum"
+
+console.log(myNum1);
+
 
 // 6. Increase the value of "myNum" by 3
 
+let myNum1 += 3;
+
+
 // 7. Output/print/return the value of "myNum"
+
+console.log(myNum1);
+
 
 // 8. Create another variable called "myOtherNum" with value 5
 
+let myOtherNum = 5;
+
+
 // 9. Compare "myNum" and "myOtherNum". Are they equal? Your code should return 'true' or 'false'
+
+myNum === myOtherNum;
 
 // 10. Increase the value of "myOtherNum" by 5
 
+let myOtherNum += 5;
+console.log(myOtherNum;)
+
+
 // 11. Compare "myNum" and "myOtherNum". Are they equal? Your code should return 'true' or 'false'
 
+myNum === myOtherNum;
+
 // 12. Create a third variable called "myString" with value "10" (note: "2" should be a string, NOT a number)
+
+let myString = 10;
+console.log(myString);
--- Assignments/Unit_2/Assignment_2/index.js
+++ Assignments/Unit_2/Assignment_2/index.js
@@ -1,5 +1,33 @@
-// Write Javascript to perform the following tasks. Please submit links to your assignment on repl.it. Here is the Javascript console we used in class: repl.it
+/*
+Write Javascript to perform the following tasks. 
+Please push your changes back to your personal OR_MTF_Submissions directory. 
+*/
+
+// USE JavaSCript to complege these tasks:
 
 // 1. Change the background color of The first page by selecting the appropriate ID.
 
-// 2.
+document.body.style.backgroundColor = "lightblue";
+
+// 2. Change the inner text of the h1 to say anything that you would like.
+
+document.getElementById("theTitle").innerHTML = "Good night";
+
+// 3. Create a variable that is equal to the DOM object that has the id random-btn.
+
+document.getElementById("random-btn").innerHTML = "Random Button for Font Size";
+
+// 4. Create a variable that is equal to the DOM object that has the id pageTitle. 
+
+document.getElementById("pageTitle").innerHTML = "10:54PM";
+
+
+// 5. Create a fucntion called randomSize that changes the font size of pageTitle to be between 1px and 100px.
+
+function randomSize() {
+	document.getElementById("theTitle").style.fontSize = Math.floor((Math.random() * 100) + 1)+"px";
+}
+
+// 6. Create an event listener that runs the randomSize function on click.
+
+document.getElementById("random-btn").addEventListener("click", randomSize);
